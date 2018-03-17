a2e1cc8730457fc6fca11b70a73b4458ca04c9b8
Lucio Rogerio SP <lucio.rogerio.senior@gmail.com>
2018-03-17 09:12:32 -0300

    Initial Upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RegeditRecursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<string> lstKeyPath = RegeditUtilities.FindKey(Registry.LocalMachine.OpenSubKey("SOFTWARE", false), "4.0.30319.0");
+
+                if (lstKeyPath.Any())
+                {
+                    Console.WriteLine("{0} keys founded.", lstKeyPath.Count);
+
+                    foreach (var item in lstKeyPath)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Key does not exist.");
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)  
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ RegeditUtilities.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace RegeditRecursive
+{
+    public class RegeditUtilities
+    {
+        /// <summary>
+        /// Find an text in regedit keys
+        /// </summary>
+        /// <param name="MasterKey">Parent Key</param>
+        /// <param name="key">Key name to find</param>
+        /// <returns></returns>
+        public static List<string> FindKey(RegistryKey MasterKey, string key)
+        {
+            List<string> lstKeysFound = new List<string>();
+
+            FindKeyinRegistryKey(MasterKey, key, ref lstKeysFound);
+
+            return lstKeysFound;
+        }
+
+        /// <summary>
+        /// Recursive function to find keys and increment list.
+        /// </summary>
+        /// <param name="MasterKey">Parent Key</param>
+        /// <param name="key">Key name to find</param>
+        /// <param name="lstKeysFound">List to increment</param>
+        private static void FindKeyinRegistryKey(RegistryKey MasterKey, string key, ref List<string> lstKeysFound)
+        {
+            string[] keys = MasterKey.GetSubKeyNames();
+            foreach (string itemKey in keys)
+            {
+                try
+                {
+                    if (itemKey.Contains(key))
+                    {
+                        lstKeysFound.Add(MasterKey.Name + " - " + itemKey);
+                        break;
+                    }
+
+                    FindKeyinRegistryKey(MasterKey.OpenSubKey(itemKey), key, ref lstKeysFound);                                        
+                }
+                catch // Prevent access denied
+                { }
+                
+            }
+        }
+    }
+}
